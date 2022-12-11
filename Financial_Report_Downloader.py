9c454ee3faef3c3a795078a2bbfc983e49d234e1
SiHoonChris <chrisleegpock@gmail.com>
2022-12-11 17:17:53 +0900

    12-11-2022

--- Financial_Report_Downloader.py
+++ Financial_Report_Downloader.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-headers="Mozilla/5.0"
+headers="Mozilla/5.0" # user-agent 전체 안써도 문제 없음
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 options = webdriver.ChromeOptions()
@@ -10,51 +10,67 @@ import time
 import datetime
 
 
-class Date_Sequence_Error(Exception):
-    def __init__(self, msg):
-        self.msg=msg
-    def __str__(self):
-        return self.msg
+# ---------- 조회 기간 입력 / SearchDate() ---------- #
+From_date=datetime.date.today
+To_date=datetime.date.today
 
-while True:
-    try:
-        example="<입력 예시>\n\n(시작) 연 : 2021\n(시작) 월 : 12\n(시작) 일 : 20\n(종료) 연 : 2022\n(종료) 월 : 3\n(종료) 일 : 5"
-        print(example+"\n-----------------")
-        fy=input("(시작) 연 : ")
-        fm=input("(시작) 월 : ")
-        fd=input("(시작) 일 : ")
-        ty=input("(종료) 연 : ")
-        tm=input("(종료) 월 : ")
-        td=input("(종료) 일 : ")
-        From_date=datetime.date(int(fy),int(fm),int(fd))
-        To_date=datetime.date(int(ty),int(tm),int(td))
-        Today=datetime.date.today()
+def SearchDate():
+    class Date_Sequence_Error(Exception):
+        def __init__(self, msg):
+            self.msg=msg
+        def __str__(self):
+            return self.msg
 
-        if str(To_date) < str(From_date) :
-            raise Date_Sequence_Error("*** '시작 날짜'가 '종료 날짜' 보다 늦음 ***\n")
-        if str(Today) < str(From_date):
-            raise Date_Sequence_Error("*** '시작 날짜'가 '오늘' 이후 임 ***\n")
-        if str(Today) < str(To_date):
-            raise Date_Sequence_Error("*** '종료 날짜'가 '오늘' 이후 임 ***\n")
+    while True:
+        try:
+            example="<입력 예시>\n\n(시작) 연 : 2021\n(시작) 월 : 12\n(시작) 일 : 20\n(종료) 연 : 2022\n(종료) 월 : 3\n(종료) 일 : 5\n"
+            print(example+"\n----------------------")
 
-        break
-    except ValueError:
-        print("*** 입력 내용 다시 확인! - 문자나 기호 X / <입력 예시> 참고 ***")
-    except Date_Sequence_Error as err:
-        print(err)
+            print("<조회 기간 입력>\n")
+            from_year=input("(시작) 연 : ")
+            from_month=input("(시작) 월 : ")
+            from_day=input("(시작) 일 : ")
+            to_year=input("(종료) 연 : ")
+            to_month=input("(종료) 월 : ")
+            to_day=input("(종료) 일 : ")
+            Today=datetime.date.today()
+            global From_date
+            From_date=datetime.date(int(from_year),int(from_month),int(from_day))
+            global To_date
+            To_date=datetime.date(int(to_year),int(to_month),int(to_day))
+
+            if str(To_date) < str(From_date) :
+                raise Date_Sequence_Error("*** '시작 날짜'가 '종료 날짜' 보다 늦음 ***\n")
+            if str(Today) < str(From_date):
+                raise Date_Sequence_Error("*** '시작 날짜'가 '오늘' 이후 임 ***\n")
+            if str(Today) < str(To_date):
+                raise Date_Sequence_Error("*** '종료 날짜'가 '오늘' 이후 임 ***\n")
+
+            print("\n----------------------")
+
+            break
+        except ValueError:
+            print("*** 입력 내용 다시 확인! - 문자나 기호 X / <입력 예시> 참고 ***")
+        except Date_Sequence_Error as err:
+            print(err)
+# ---------- 조회 기간 입력 / SearchDate() ---------- #
 
 
-def us_report(*CIKs):
+# ---------- 미국 주식 공시자료 다운로드 / US_Report() ---------- #
+def US_Report(*CIKs):
 
     browser=webdriver.Chrome(options=options)
     CNTs=0
+
+    print("[해외 : 미국]")
+    print(f"START ( 종목 : {len(CIKs)} )")
+    print("-"*100)
 
     for CIK in CIKs:
         url = "https://www.sec.gov/edgar/browse/?CIK={}&owner=exclude".format(CIK)
         browser.get(url)
         time.sleep(2)
         browser.find_element(By.XPATH, "//*[@id='btnViewAllFilings']").click()
-            # browser.find_element_by_xpath("//*[@id='btnViewAllFilings']").click()
         time.sleep(2)
 
         soup = BeautifulSoup(browser.page_source, "lxml")
@@ -87,39 +103,37 @@ def us_report(*CIKs):
         
         print("-"*100)
 
-    print(f"----- END ( 종목 : {len(CIKs)}, 내용 : {CNTs} ) -----\n")
+    print(f"END ( 종목 : {len(CIKs)}, 내용 : {CNTs} )\n")
     browser.quit()
+# ---------- 미국 주식 공시자료 다운로드 / US_Report() ---------- #
 
 
-def kr_report(*CODEs):
+# ---------- 한국 주식 공시자료 다운로드 / KR_Report() ---------- #
+def KR_Report(*CODEs):
 
     browser=webdriver.Chrome(options=options)
     if_num=0
     else_num=0
+
+    print("[국내 : 한국]")
+    print(f"START ( 종목 : {len(CODEs)} )")
+    print("-"*100)
 
     for CODE in CODEs:
         url="https://dart.fss.or.kr/"
         browser.get(url)
         time.sleep(2)
         browser.find_element(By.XPATH, "//*[@id='textCrpNm2']").send_keys(CODE)
-            # browser.find_element_by_xpath("//*[@id='textCrpNm2']").send_keys(CODE)
         browser.find_element(By.XPATH, "//*[@id='searchForm2']/div[1]/div[3]/a").click()
-            # browser.find_element_by_xpath("//*[@id='searchForm2']/div[1]/div[3]/a").click()
         time.sleep(2)
         Start_date=str(From_date).replace("-", "")
         End_date=str(To_date).replace("-", "")
         browser.find_element(By.XPATH, "//*[@id='startDate']").clear()
-            # browser.find_element_by_xpath("//*[@id='startDate']").clear()
         browser.find_element(By.XPATH, "//*[@id='startDate']").send_keys(Start_date)
-            # browser.find_element_by_xpath("//*[@id='startDate']").send_keys(Start_date)
         browser.find_element(By.XPATH, "//*[@id='endDate']").clear()
-            # browser.find_element_by_xpath("//*[@id='endDate']").clear()
         browser.find_element(By.XPATH, "//*[@id='endDate']").send_keys(End_date)
-            # browser.find_element_by_xpath("//*[@id='endDate']").send_keys(End_date)
         browser.find_element(By.XPATH, "//*[@id='maxResultsCb']/option[4]").click()
-            # browser.find_element_by_xpath("//*[@id='maxResultsCb']/option[4]").click()
         browser.find_element(By.XPATH, "//*[@id='searchForm']/div[2]/div[2]/a[1]").click()
-            # browser.find_element_by_xpath("//*[@id='searchForm']/div[2]/div[2]/a[1]").click()
         time.sleep(2)
 
         soup = BeautifulSoup(browser.page_source, "lxml")
@@ -148,7 +162,6 @@ def kr_report(*CODEs):
 
         else:
             browser.find_element(By.XPATH, f"//*[@id='psWrap']/div[2]/ul/li[{len(pages)}]/a").click()
-                # browser.find_element_by_xpath(f"//*[@id='psWrap']/div[2]/ul/li[{len(pages)}]/a").click()
             time.sleep(2)
             soup = BeautifulSoup(browser.page_source, "lxml")
             lists_on_lastpg=soup.find("tbody").find_all("tr")
@@ -157,7 +170,6 @@ def kr_report(*CODEs):
 
             for pg in range(1, len(pages)+1):
                 browser.find_element(By.XPATH, f"//*[@id='psWrap']/div[2]/ul/li[{pg}]/a").click()
-                    # browser.find_element_by_xpath(f"//*[@id='psWrap']/div[2]/ul/li[{pg}]/a").click()
                 
                 time.sleep(2)
                 soup = BeautifulSoup(browser.page_source, "lxml")
@@ -189,17 +201,23 @@ def kr_report(*CODEs):
     total_num = if_num + else_num
     print(f"----- END ( 종목 : {len(CODEs)}, 내용 : {total_num} ) -----\n")
     browser.quit()
+# ---------- 한국 주식 공시자료 다운로드 / KR_Report() ---------- #
 
 
+# ---------- 전체(한국, 미국) 자료 다운로드 / Report_Downloader() ---------- #
 def Report_Downloader():
+    SearchDate()
     start_time=time.time()
-    us_report("320193")# "1652044" #, "1018724", "1652044", "200406", "21344", "936468", "789019", "19617")
-              # AAPL,     AMZN,      GOOGL,    JNJ,      KO,       LMT,     MSFT,     JPM
-    kr_report("005930")  # 삼성전자
+    US_Report("320193", "1652044")  # AAPL , GOOGL
+    KR_Report("005930", "000660")   # 삼성전자 , SK하이닉스
     end_time=time.time()
     time_spent = end_time - start_time
     print(f"({str(round((time_spent), 4))} sec.)")
+# ---------- 전체(한국, 미국) 자료 다운로드 / Report_Downloader() ---------- #
 
 
+
+# ---------- 전체(한국, 미국) 자료 다운로드 실행 ---------- #
 if __name__=="__main__":
     Report_Downloader()
+# ---------- 전체(한국, 미국) 자료 다운로드 실행 ---------- #
